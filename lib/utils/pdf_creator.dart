faa78a18ea167b6291fe14f35e036342ca702f60
Paulo Henrique Ortolan <portolan@mercadona.com>
2026-02-20 16:16:43 +0000

    PDF generation

--- lib/utils/pdf_creator.dart
+++ lib/utils/pdf_creator.dart
@@ -1,9 +1,12 @@
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 import '../data/todo.dart';
 
 Future<pw.Document> generatePDFReport(List<Todo> todos) async {
   final pdf = pw.Document();
+
+  List<Todo> undone = todos.where((todo) => !todo.done).toList();
 
   pdf.addPage(
     pw.Page(
@@ -13,6 +16,17 @@ Future<pw.Document> generatePDFReport(List<Todo> todos) async {
           child: pw.Column(
             children: [
               _buildHeader(),
+              pw.Expanded(
+                child: pw.Padding(
+                  padding: pw.EdgeInsets.symmetric(horizontal: 24),
+                  child: pw.Column(
+                    children: List.generate(
+                      undone.length,
+                      (u) => _buildTaskRow(undone[u]),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -32,6 +46,28 @@ pw.Widget _buildHeader() {
         fontSize: 18,
         fontWeight: pw.FontWeight.bold,
       ),
+    ),
+  );
+}
+
+pw.Widget _buildTaskRow(Todo todo) {
+  return pw.Container(
+    child: pw.Row(
+      children: [
+        pw.Container(
+          width: 10,
+          height: 10,
+          decoration: pw.BoxDecoration(
+            color: PdfColors.white,
+            border: pw.Border.all(
+              color: PdfColors.black,
+              width: 1.5,
+            ),
+          ),
+        ),
+        pw.SizedBox(width: 20),
+        pw.Text(todo.task),
+      ],
     ),
   );
 }
